54a9ee6e982264e3ffb729921100c326160a8774
Nikhil <nikhil.b.k@gmail.com>
2022-03-18 13:23:43 -0700

    Update a_b_sqrt2.cc
    finished rest of program

--- a_b_sqrt2.cc
+++ a_b_sqrt2.cc
@@ -20,3 +20,30 @@ struct Number {
 };
 
 vector<double> GenerateFirstKABSqrt2(int k) { 
+  // Initial for 0 + 0 * sqrt(2).
+  set<Number, function<bool(Number, Number)>> candidates ( 
+    {{0, 0}}, [](const Number &a, const Number &b) { return a.val < b.val; });
+  
+  vector<double> result;
+  while (size(result) < k) { 
+    auto next_smallest = cbegin(candidates);
+    result.emplace_back(next_smallest->val);
+    
+    //Adds the next two numbers derived from next_smallest.
+    candidates.emplace(next_smallest->a +1, next_smallest->b);
+    candidates.emplace(next_smallest->a, next_smallest->b +1);
+    cansidates.erase(next_smallest);
+  }
+  return result;
+}
+
+int main(int argc, int char *argv[]) {
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"k"};
+  return GenericTestMain(args, "a+b+sqrt2.tsv",
+                         &GenerateFirstKABSqrt2, DefaultCOmparator{},
+                         param_names);
+}
+
+  
+    

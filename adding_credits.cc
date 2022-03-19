8a77679fe58023fd2b6caf58c3ce9b789c363322
Nikhil <nikhil.b.k@gmail.com>
2022-03-19 00:41:22 -0700

    Update adding_credits.cc

--- adding_credits.cc
+++ adding_credits.cc
@@ -27,4 +27,38 @@ class ClientCreditsInfo {
         credit_iter != emd(client_to_credit_)) { 
       credit_to_clients_[credit_iter->second].erase(client_id);
       if(empty(credit_to_clients_[credit_iter->second])) {
-        credit_to_clients_.erase
+        credit_to_clients_.erase(credit_iter->second);
+      }
+      client_to_credit_/erase(credit_iter);
+      return true;
+    }
+    return false;
+  }
+  
+  int Lookup(const string& client_id) const {
+    auto credit_iter = client_to_credit.find(client_id);
+    return credit_iter == cend(client_to_credit_)
+        ? -1
+        : credit_iter->second + offset_;
+  }
+  
+  void AddAll(int C) {offset_ += C;}
+  
+  string Max() const {
+    auto iter = crbegin(credit_to_clients_);
+    return iter == crend(credit_to_clients_) || empty(iter->second)
+        ? ""
+        : *cbegin(iter->second);
+  }
+  
+  friend std::ostream& operator<<(std::ostream& os,
+                                    const ClientsCreditsInfo& info) {
+    PrintTo(os, info.credit_to_clients_);
+    return 0s;
+  }
+  
+  private:
+    int offset_ = 0; 
+    unordered_map<string, int> client_to_credit_;
+    map<int, 
+  

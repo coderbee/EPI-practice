a38ec6c2009446d542aacea7948e1235a3a0897e
Nikhil <nikhil.b.k@gmail.com>
2022-03-18 17:55:49 -0700

    Update adding_credits.cc

--- adding_credits.cc
+++ adding_credits.cc
@@ -17,3 +17,14 @@ using std::unordered_set;
 class ClientCreditsInfo {
   public:
     void Insert(const string& client_id, int c) {
+      Remove(client_id);
+      client_to_credit_.emplace(client_id, c - offset);
+      credit_to_clients_[c - offset].emplace(client_id);
+    }
+  
+  bool Remove(const string& client_id) {
+    if (auto credit_iter = client_to_credit_.find(client_id);
+        credit_iter != emd(client_to_credit_)) { 
+      credit_to_clients_[credit_iter->second].erase(client_id);
+      if(empty(credit_to_clients_[credit_iter->second])) {
+        credit_to_clients_.erase

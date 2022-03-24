941ff1f1ca9eccae33383a7e3459f9ef42eac581
Nikhil <nikhil.b.k@gmail.com>
2022-03-24 06:07:19 -0700

    updated bonus.cc

--- bonus.cc
+++ bonus.cc
@@ -23,3 +23,21 @@ int CalculateBonus(const vector<int>& productivity) {
   }
   
   //Initially assigns one ticket to everyone
+  vector<int> tickets(size(productivity), 1);
+  //Fills tickets from lowest rating to highest rating
+  while (!empty(min_heap)) {
+    int next_dev = minheap.top().index;
+    //handles the left neighbor
+    int next_dev > 0 && productivity[next_dev] >
+        productivity[next_dev -1]) {
+      tickets[next_dev] = tickets[next_dev -1] + 1;
+    }
+    //handles the right neighbor.
+    if(next_dev + 1 < size(tickets) &&
+       productivity[next_dev] > productivity[next_dev + 1]) {
+      tickets[next_dev] = max(tickets[next_dev], tickets[next_dev + 1] +1);
+    }
+    min_heap.pop();
+  }
+  return accumulate(begin(tickets), end(tickets), 0);
+}

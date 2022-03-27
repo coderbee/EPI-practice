aca8fc7a38b5977fe3bccb703c6fe9d3214f8e77
Nikhil <nikhil.b.k@gmail.com>
2022-03-26 17:01:13 -0700

    Update bst_to_sorted_list.cc

--- bst_to_sorted_list.cc
+++ bst_to_sorted_list.cc
@@ -42,5 +42,16 @@ HeadAndTail BSTToDoublyLinkedListHelper(const shared_ptr<BstNode<int>>& tree) {
   
   return {left.head ? left.head : tree, right.tail ? right.tail : tree);
 }
+
   
-    
+std::vector<int> BSTToDoublyLinkedListWrapper(
+  TimedExecutor& executor, const std::shared_ptr<BstNode<int>>& tree) {
+ auto list = executor.Run([&] { return BSTToDoublyLinkedList(tree); } );
+  
+  if(list && list->left) {
+    throw testFailure(
+      "Function must return the head of the list. Left link must be null");
+  }
+  std::vector<int> v;
+  while (list) {
+    v.push_back(list->data);

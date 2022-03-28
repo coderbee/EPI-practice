1bb71d02875ac86852d706a95962001dae0425bc
Nikhil <nikhil.b.k@gmail.com>
2022-03-28 07:45:49 -0700

    Create buy_and_sell_stock_k_times.cc

---
+++ buy_and_sell_stock_k_times.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+#include <limits>
+
+using std::max;
+using std::min;
+using std::numeric_limits;
+using std::vector;
+
+double UnlimitedPairsProfits(const vector<double>& );
+
+double BuyAndSellStockKTimes(const vector<double>& prices, int k) {
+  

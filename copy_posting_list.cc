70a7d2cf125ae63930846f8013a629330c923a4c
Nikhil <nikhil.b.k@gmail.com>
2022-03-31 15:45:52 -0700

    Update copy_posting_list.cc

--- copy_posting_list.cc
+++ copy_posting_list.cc
@@ -5,4 +5,16 @@
 using std::make_shared;
 using std::shared_ptr;
 
-shared_ptr<PostingListNode>
+shared_ptr<PostingListNode> CopyPostingsList(
+  const shared_ptr<PostingListNode>& l) {
+  if( l ==nullptr) {
+    return nullptr;
+  }
+  
+  //Stage 1: Makes a copy of the original list without assigning the jump
+  //          field, and creates the mapping for each node in the original
+  //          list to the copied list.
+  auto iter = l;
+  while (iter) {
+    auto new_node = 
+      

602c1b2595471e1455f21b042ba3bf07558993d6
Nikhil <nikhil.b.k@gmail.com>
2022-04-03 14:29:51 -0700

    Create defective_jugs.cc

---
+++ defective_jugs.cc
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <memory>
+#include <unordered_set>
+#include <vector>
+
+using std::make_unique;
+using std::unordered_set;
+using std::vector;
+
+struct Jug;
+struct VolumeRange;
+struct HashVolumeRange;
+
+bool CheckFeasibleHelper(const vector<Jug>&, int, int,
+                         unordered_set<VolumeRange, HashVolumeRange>*);
+
+struct Jug {
+  int low, high;
+};
+
+struct VolumeRange {
+  int low, high;
+
+  bool operator==(const VolumeRange& that) const {
+    return low == that.low && high == that.high;
+  }
+};
+
+struct HasVolumeRange {
+  size_t operator() (const VolumeRange& p) const {
+    return static_cast<size_t>(31) * p.low + p.high;
+  }
+};
+
+bool CheckFeasible(const vector<Jug>& jugs, int L, int H) {
+  return CheckFeasibleHelper(
+    jugs, L, H,
+    make_unique<unordered_set<VolumeRange, HasVolumeRange>>().get());
+}
+
+bool CheckFeasibleHelper(const vector<Jug>& jugs, int L, int H,
+                         unordered_set<VolumeRange, HasVolumeRange>* c) {
+  if( L > H || c->count({L,H}) || (L < 0 && H < 0)) {
+    return false;
+  }
+  
+  //Checks the volume for each jug to see if it is possible.
+  if(any_of(begin(jugs), end(jugs), [&](const Jug& j) {
+    return (L <= j.low && j.high <= H) ||
+      CHeckFeasibleHelper(jugs, L - j.low, H - j.high, c);
+  })) {
+    return true;
+  }
+  c->emplace(VolumeRange{L, H}); //Marks this as impossible.
+  return false;
+}
+
+ 
+     

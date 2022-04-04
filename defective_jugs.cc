6d0692ccdb81be10db458f76c563feb2ca3d69e7
Nikhil <nikhil.b.k@gmail.com>
2022-04-03 18:59:28 -0700

    Update defective_jugs.cc

--- defective_jugs.cc
+++ defective_jugs.cc
@@ -55,5 +55,14 @@ bool CheckFeasibleHelper(const vector<Jug>& jugs, int L, int H,
   return false;
 }
 
+namespace test_framework {
+  template <>
+  struct SerializationTrait<Jug> : UserSerTrait<Jug, int, int> {};
+} //namespace test_framework
+
+bool operator==(const Jug& lhs, const Jug& rhs) {
+  return lhs.low == rhs.low && lhs.high == rhs.high;
+}
+
  
      

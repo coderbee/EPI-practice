0f2b3c24d2bb8ee0393f6976bff898d05f632a7b
Nikhil <nikhil.b.k@gmail.com>
2022-04-13 20:34:23 -0700

    Update fibonacci.cc

--- fibonacci.cc
+++ fibonacci.cc
@@ -1,1 +1,15 @@
 #include <unordered_map>
+
+using std::unordered_map;
+
+unordered_map<int, int> cache;
+
+int Fibonacci(int n) {
+  if(n <=1) {
+    return n;
+  } else if (!cache.count(n)) {
+    cache[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+  }
+  return cache[n];
+}
+

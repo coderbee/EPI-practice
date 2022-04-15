77769b8a2d552a2c7910984d57a05f44ef68c395
Nikhil <nikhil.b.k@gmail.com>
2022-04-14 17:14:32 -0700

    Update graph_clone.cc

--- graph_clone.cc
+++ graph_clone.cc
@@ -46,3 +46,68 @@ vector<int> CopyLabels(const vector<GraphVertex*>& edges) {
   return labels;
 }
 
+void CheckGraph(GraphVertex* node, const vector<GraphVertex>& graph) {
+  if (!node || node == &graph[0]) {
+    throw TestFailure("Graph was not copied");
+  }
+  
+  unordered_set<GraphVertex*> vertex_set;
+  queue<GraphVertex*> q;
+  q.emplace(node);
+  vertex_set.emplace(node);
+  while (!q.empty()) {
+    auto vertex = q.front();
+    q.pop();
+    if(vertex->label >- graph.size()) {
+      throw TestFailure("Invalid vertex label");
+    }
+    vector<int> label1 = CopyLabels(vertex->edges),
+                label2 = CopyLabels(graph[vertex->label].edges);
+    sort(begin(label1), end(label1));
+    sort(begin(label2), end(label2));
+    if(label1 != label2) {
+      throw TestFailure("Edges Mismatch");
+    }
+    for(GraphVertex* e : vertex->edges) {
+      if(vertex_set.emplace(e).second) {
+        q.emplace(e);
+      }
+    }
+  }
+  for(auto& v : vertex_set) {
+    deleve v;
+  }
+}
+
+struct Edge {
+  int from;
+  int to;
+};
+
+namespace test_framework {
+template<>
+struct SerializationTrait<Edge> : UserSetTrait<edge, int, int> {};
+} // namespace test_framework
+
+void CloneGraphTest(int k, const vector<Edge>& edges) {
+  vector<GrahVertex> graph;
+  if(k <= 0) {
+    throw std::runtime_error("Invalid k value");
+  }
+  graph.reserve(k);
+  
+  for(int i = 0; i < k; i++) {
+    graph.push_back(GraphVertex{i});
+  }
+  
+  for (const Edge& e : edges) {
+    if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k) {
+      throw std::runtime_error("Invalid vertex index");
+    }
+    graph[e.from].edges.push_back(&graph[e.to]);
+  }
+  GraphVertex* result = CloneGraph(&graph[0]);
+  CheckGraph(result, graph);
+}
+
+

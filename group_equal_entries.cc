2569497fea6987ff8a2bde27fbede7f6ed32d30c
Nikhil <nikhil.b.k@gmail.com>
2022-04-17 08:30:45 -0700

    Create group_equal_entries.cc

---
+++ group_equal_entries.cc
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::function;
+using std::max;
+using std::string;
+using std::swap;
+using std::unprdered_map;
+using std::vector;

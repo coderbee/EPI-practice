61cb473e41d4db6a64cb2a45f49d0a86285fd36d
Nikhil <nikhil.b.k@gmail.com>
2022-04-18 17:54:00 -0700

    Update group_equal_entries.cc

--- group_equal_entries.cc
+++ group_equal_entries.cc
@@ -24,4 +24,25 @@ void GroupByAge(vector<Person>* people) {
     ++age_to_offset[age] = offset;
     offset += count;
   }
+  unordered_map<int, int> age_to_offset;
+  int offset = 0;
+  for (const auto& [age, count] : age_to_count) {
+    age_to_offset[age] = offset;
+    offset += count;
+  }
   
+  while (!empty(age_to_offset)) {
+    auto from = begin(age_to_offset);
+    auto to = age_to_offset.find((*people)[from->second].age);
+    swap((*people)[from->second], (*people)[to->second]);
+    // Use age_to_count to see when we are finished with a particular age.
+    --age_to_count[to->first];
+    if(age_to_count[to->first] > 0) {
+      ++to->second;
+    } else {
+      age_to_offset.erase(to);
+    }
+  }
+}
+
+

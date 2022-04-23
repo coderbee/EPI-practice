4a77e2975f4e738de8e33ef065fc05341203062d
Nikhil <nikhil.b.k@gmail.com>
2022-04-23 13:41:04 -0700

    Update hanoi.cc

--- hanoi.cc
+++ hanoi.cc
@@ -33,5 +33,30 @@ void ComputeTowerHanoiSteps(int num_rings_to_move,
                           to_peg, result_ptr);
     pegs[to_peg].push(pegs[from_peg].top());
     pegs[from_peg].pop();
-    result_ptr->
+    result_ptr->emplace_back(vector<int>{from_peg, to_peg});
+    ComputeHowerHanoiSteps(num_rings_to_move - 1, pegs, use_peg, to_peg,
+                           from_peg, result_ptr);
+  }
+}
+
+void ComputeTowerHanoiWrapper(TImedExecutor& executor, int num_rings) {
+  array<stack<int>, kNumPegs> pegs;
+  for(int i = num_rings; i >= 1; --i) {
+    pegs[0].push(i);
+  }
+  
+  vector<vector<int>> result = 
+    executor.Run([&] { return ComputeTowerHanoi(num_rings); });
+  
+  for(const <vector<int>& operation : result) {
+    int from_peg = operation[0], to_peg = operation[1];
+    if(!pegs[to_peg].empty() && pegs[from_peg].top() >= pegs[to_peg].top()) {
+      throw TestFailure("Illegal move from" + 
+                        std::to_string(pegs[from_peg].top()) + " to " +
+                        std::to_string(pegs[to_peg].top()));
+    }
+    pegs[to_peg]push(pegs[from_peg].top());
+    pegs[from_peg].pop();
+  }
+  array<stack<int>, kNumPegs> expected_pegs1, expected_pegs2;
   

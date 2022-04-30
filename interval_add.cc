bb06239d0d81e8e5650d0cd2605dd485e91708a4
Nikhil <nikhil.b.k@gmail.com>
2022-04-30 11:07:39 -0700

    Update interval_add.cc

--- interval_add.cc
+++ interval_add.cc
@@ -13,5 +13,14 @@ vector<Interval> AddInterval(const vector<Interval>& disjoint_intervals,
                              Interval new_interval) {
 size_t i = 0;
 vector<Interval> result;
-  
+
+//Process intervals in disjoint_intervals which come becore new_interval.
+while (i < size(disjoint_intervals) &&
+       new_interval.left > disjoint_intervals[i].right) {
+  result.emplace_back(disjoint_intervals[i++]);
+}
+
+//Process intervals in disjoint_intervals which overlap with
+// new_interval.
+while (i < size(disjoint_intervals) &&
   

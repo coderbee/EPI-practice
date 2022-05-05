8bf19d6b8d0063cadb7ada06392e512cec9a4abf
Nikhil <nikhil.b.k@gmail.com>
2022-05-05 16:29:44 -0700

    Update interval_add.cc

--- interval_add.cc
+++ interval_add.cc
@@ -22,5 +22,18 @@ while (i < size(disjoint_intervals) &&
 
 //Process intervals in disjoint_intervals which overlap with
 // new_interval.
-while (i < size(disjoint_intervals) &&
+while (i < size(disjoint_intervals) && 
+       new_interval.right >= disjoint_intervals[i].left) {
+  // If [a, b] and [c, d] overlap, their union is [min(a, c), max(b, d)].
+  new_interval = {min(new_interval.left, disjoint_intervals[i].left),
+                  max(new_interval.right, disjoint_intervals[i].right)};
+  ++i;
+}
+result.emplace_back(new_interval);
+//Pricesses intervals in disjoint_intervals which come after new_interval.
+result.insert(end(result), begin(disjoint_intervals) + i,
+              end(disjoint_intervals));
+return result;
+}
+
   
